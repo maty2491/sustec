e2373838e01bd2613cd32adf1e12ab30ad33a59d
Matias <maty_2491@hotmail.com>
2024-07-01 20:43:37 -0300

    Cambios minimos

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ function App() {
       <div className='row'>
         <div className='col-12 margin'>
           <img src={logo} alt="" className='d-flex mx-auto img-fluid' />
-          <h1 className='text-center'>EN CONSTRUCCIÓN</h1>
+          <hr />
+          <h1 className='text-center fw-bold'>EN CONSTRUCCIÓN</h1>
           <br />
-          <h2 className='text-center'>ESTA PÁGINA ESTARÁ LISTA EN CUALQUIER MOMENTO</h2>
+          <h2 className='text-center fs-3'>ESTA PÁGINA ESTARÁ LISTA EN CUALQUIER MOMENTO</h2>
           <br />
-          <p className='text-center'>Te invitamos a que nos contactes: info@sustec.com.ar</p>
+          <p className='text-center fs-5'>Te invitamos a que nos contactes: <span className='fw-bold'>info@sustec.com.ar</span></p>
         </div>
       </div>
 
