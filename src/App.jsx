0f28cec2508cc14d433664120012c9399bbc613b
Matias <maty_2491@hotmail.com>
2024-07-01 20:29:12 -0300

    Primera subida

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import logo from './assets/sustec.png'
+
+function App() {
+
+  return (
+    <>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12 margin'>
+          <img src={logo} alt="" className='d-flex mx-auto img-fluid' />
+          <h1 className='text-center'>EN CONSTRUCCIÓN</h1>
+          <br />
+          <h2 className='text-center'>ESTA PÁGINA ESTARÁ LISTA EN CUALQUIER MOMENTO</h2>
+          <br />
+          <p className='text-center'>Te invitamos a que nos contactes: info@sustec.com.ar</p>
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default App
